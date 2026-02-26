f51b0c9f1a7a0b30d4419c26347ae77b2203b2ee
Priyanshu Paliwal <priyanshupaliwal@cyntexa.com>
2026-02-26 18:02:47 +0530

    push

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,4 @@
 import axios from "axios";
-import React, { use } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { API_BASE_URL } from "../utils/constants";
