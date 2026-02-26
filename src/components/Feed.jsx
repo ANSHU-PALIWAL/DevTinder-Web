9c52aedb8781e4f5d6952c802746b24efdecc1d8
Priyanshu Paliwal <priyanshupaliwal@cyntexa.com>
2026-02-26 19:02:36 +0530

    Crested Feed & user Card

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect } from "react";
+import { API_BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+import UserCard from "./UserCard";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const feed = useSelector((store) => store.feed);
+  const dispatch = useDispatch();
 
-export default Feed
+  const getFeed = async () => {
+    try {
+      if (feed) return;
+      const res = await axios.get(API_BASE_URL + "/feed", {
+        withCredentials: true,
+      });
+      dispatch(addFeed(res.data));
+    } catch (error) {
+      console.error("Error fetching feed:", error);
+    }
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
+
+  return (
+    feed && (
+      <div className="min-h-screen flex items-center justify-center">
+        <UserCard user={feed?.data?.[0]} />
+      </div>
+    )
+  );
+};
+
+export default Feed;
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const UserCard = ({ user }) => {
+  console.log("userrrr", user);
+  const { firstName, lastName, photoUrl, age, gender, about, skills } = user;
+  return (
+    <div className="card bg-base-300 w-96 shadow-sm">
+      <figure>
+        <img src={user.photoUrl} alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          {firstName} {lastName}
+        </h2>
+        <p>{about}</p>
+
+        {age && gender && (
+          <p>
+            {age}, {gender}
+          </p>
+        )}
+
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Ignore</button>
+          <button className="btn btn-secondary">Intrested</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
